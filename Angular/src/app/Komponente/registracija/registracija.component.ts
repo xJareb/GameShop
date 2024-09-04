34ac870dbb1758d3c65ebb29e5f9024009f4593b
Admir Jareb <110131840+xJareb@users.noreply.github.com>
2024-09-04 12:54:45 +0200

    Task 4 - Create validation for registration fields

--- Angular/src/app/Komponente/registracija/registracija.component.ts
+++ Angular/src/app/Komponente/registracija/registracija.component.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import {Component, input, OnInit} from '@angular/core';
+import {CommonModule, NgOptimizedImage} from "@angular/common";
 import {Router, RouterLink} from "@angular/router";
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {MojConfig} from "../../moj-config";
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-registracija',
@@ -14,36 +14,109 @@ import {FormsModule} from "@angular/forms";
     RouterLink,
     NgbTooltipModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   templateUrl: './registracija.component.html',
   styleUrl: './registracija.component.css',
   host: { class: 'd-block' }
 })
-export class RegistracijaComponent implements OnInit{
+export class RegistracijaComponent implements OnInit {
 
-  ngIme:any;
-  ngPrezime:any;
-  ngKorisnickoIme:any;
-  ngEmail:any;
-  ngDatumRodjenja:any;
-  ngLozinka:any;
-  constructor(public httpClient:HttpClient,private router:Router) {
+  userForm: FormGroup;
+
+  constructor(public httpClient: HttpClient, private router: Router) {
+    this.userForm = new FormGroup({
+      ime: new FormControl("", [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])[A-Za-z]{4,}$/)]),
+      prezime: new FormControl("", [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])[A-Za-z]{4,}$/)]),
+      korisnickoIme: new FormControl("", [Validators.required, Validators.minLength(4)]),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      datumRodjenja: new FormControl("", [Validators.required]),
+      lozinka: new FormControl("", [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{7,}/)],)
+    })
   }
+
   ngOnInit(): void {
   }
+
   kreirajKorisnika() {
-    let url= MojConfig.adresa_servera + `/Dodaj`;
+    let url = MojConfig.adresa_servera + `/Dodaj`;
     let requestBody = {
-      "ime": this.ngIme,
-      "prezime": this.ngPrezime,
-      "korisnickoIme": this.ngKorisnickoIme,
-      "email": this.ngEmail,
-      "datumRodjenja": this.ngDatumRodjenja,
-      "lozinka": this.ngLozinka
+      "ime": this.userForm.controls['ime'].value,
+      "prezime": this.userForm.controls['prezime'].value,
+      "korisnickoIme": this.userForm.controls['korisnickoIme'].value,
+      "email": this.userForm.controls['email'].value,
+      "datumRodjenja": this.userForm.controls['datumRodjenja'].value,
+      "lozinka": this.userForm.controls['lozinka'].value
     }
-    this.httpClient.post(url,requestBody).subscribe(x=>{
+
+    const validnaForma = this.userForm.valid;
+    if(validnaForma){
+      this.httpClient.post(url,requestBody).subscribe(x=>{
         this.router.navigate(["/"]);
-    })
+      })
+    }
+  }
+
+  postaviStilIme(){
+    if (this.userForm.controls['ime'].invalid && !this.userForm.controls['ime'].untouched) {
+      return {
+        'background-color': 'red',
+        'color': 'white'
+      }
+    } else {
+      return {}
+    }
+  }
+  postaviStilPrezime(){
+    if (this.userForm.controls['prezime'].invalid && !this.userForm.controls['prezime'].untouched) {
+      return {
+        'background-color': 'red',
+        'color': 'white'
+      }
+    } else {
+      return {}
+    }
+  }
+  postaviStilKorisnickoIme(){
+    if (this.userForm.controls['korisnickoIme'].invalid && !this.userForm.controls['korisnickoIme'].untouched) {
+      return {
+        'background-color': 'red',
+        'color': 'white'
+      }
+    } else {
+      return {}
+    }
+  }
+  postaviStilEmail(){
+    if (this.userForm.controls['email'].invalid && !this.userForm.controls['email'].untouched) {
+      return {
+        'background-color': 'red',
+        'color': 'white'
+      }
+    } else {
+      return {}
+    }
+  }
+  postaviStilDatumRodjenja(){
+    if (this.userForm.controls['datumRodjenja'].invalid && !this.userForm.controls['datumRodjenja'].untouched) {
+      return {
+        'background-color': 'red',
+        'color': 'white'
+      }
+    } else {
+      return {}
+    }
+  }
+  postaviStilLozinka(){
+    if (this.userForm.controls['lozinka'].invalid && !this.userForm.controls['lozinka'].untouched) {
+      return {
+        'background-color': 'red',
+        'color': 'white'
+      }
+    } else {
+      return {}
+    }
   }
 }

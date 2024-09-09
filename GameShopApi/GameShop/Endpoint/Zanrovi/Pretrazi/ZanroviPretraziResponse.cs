f401d3200892be1ffbc947b87a45026bbfaa21d7
Admir Jareb <110131840+xJareb@users.noreply.github.com>
2024-09-09 14:25:19 +0200

    Task 10 - Generate endpoint for listing all categories

---
+++ GameShopApi/GameShop/Endpoint/Zanrovi/Pretrazi/ZanroviPretraziResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace GameShop.Endpoint.Zanrovi.Pretrazi
+{
+    public class ZanroviPretraziResponse
+    {
+        public List<ZanroviPretraziResponseZanr> Zanrovi {  get; set; }
+    }
+    public class ZanroviPretraziResponseZanr
+    {
+        public int ID { get; set; }
+        public string Naziv { get; set; }
+    }
+}

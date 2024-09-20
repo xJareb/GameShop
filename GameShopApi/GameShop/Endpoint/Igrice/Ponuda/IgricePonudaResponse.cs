2341e83362e345856f7eeff18e27c906572fe397
Admir Jareb <admir.jareb@edu.fit.ba>
2024-09-20 15:27:21 +0200

    Task 26 - Generate the endpoint for display highlighted games

---
+++ GameShopApi/GameShop/Endpoint/Igrice/Ponuda/IgricePonudaResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace GameShop.Endpoint.Igrice.Ponuda
+{
+    public class IgricePonudaResponse
+    {
+        public List<IgricePonudaResponseIgrice> igrice { get; set; }
+    }
+    public class IgricePonudaResponseIgrice
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public string Slika { get; set; }
+        public float PostotakAkcije { get; set; }
+        public float? AkcijskaCijena { get; set; }
+    }
+}

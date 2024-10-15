b761d23045f0163201b3cdf0db53ccf25605e1cc
Admir Jareb <admir.jareb@edu.fit.ba>
2024-10-16 01:29:49 +0200

    Task 56 - Perform validation of the payment fields

--- Angular/src/app/Komponente/korpa/korpa-placanje/korpa-placanje.component.ts
+++ Angular/src/app/Komponente/korpa/korpa-placanje/korpa-placanje.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {NgForOf, NgIf} from "@angular/common";
-import {ReactiveFormsModule} from "@angular/forms";
+import {NgForOf, NgIf, NgStyle} from "@angular/common";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {RouterLink} from "@angular/router";
 
 @Component({
@@ -10,7 +10,8 @@ import {RouterLink} from "@angular/router";
     NgForOf,
     NgIf,
     ReactiveFormsModule,
-    RouterLink
+    RouterLink,
+    NgStyle
   ],
   templateUrl: './korpa-placanje.component.html',
   styleUrl: './korpa-placanje.component.css'
@@ -21,13 +22,61 @@ export class KorpaPlacanjeComponent implements OnInit{
     @Output() otvori = new EventEmitter<boolean>();
     private prikaz: boolean = true;
 
+    userPayment : FormGroup;
+
     constructor() {
+      this.userPayment = new FormGroup({
+        brojKartice: new FormControl("",[Validators.required,Validators.pattern(/^\d{16}$/)]),
+        imePrezime: new FormControl("",[Validators.required,Validators.pattern(/^[A-Z][a-z]+ [A-Z][a-z]+$/)]),
+        datumIsteka: new FormControl("",[Validators.required]),
+        sigurnosniBroj: new FormControl("",[Validators.required,Validators.pattern(/^\d{3}$/)]),
+      })
     }
     ngOnInit(): void {
 
     }
-  zatvori() {
+    zatvori() {
     this.prikaz = !this.prikaz;
     this.otvori.emit(this.prikaz);
+    }
+    postaviStil(kontrola:string){
+    if (this.userPayment.controls[kontrola].invalid && !this.userPayment.controls[kontrola].untouched) {
+      return {
+        'background-color': 'red',
+        'color': 'white'
+      }
+    } else {
+      return {}
+    }
+    }
+    cekiranoPamcenje():boolean{
+      let checkbox = document.getElementById('exampleCheck1') as HTMLInputElement;
+      if(checkbox.checked){
+        return true;
+      }
+      return false;
+    }
+
+  placanje() {
+    const validnaForma = this.userPayment.valid;
+    if(validnaForma){
+      if(this.cekiranoPamcenje()){
+        // TODO :: pozvati endpoint za spremanje kartice u bazu (sifrirano)
+        alert('Placanje sa spremanjem')
+      }else{
+        // TODO :: sadrzaj korpe spremiti u tabelu za transakcije
+        alert('Placanje')
+      }
+
+    }
   }
+  restrikcijaUnosaSlova(event:Event){
+      const unos = event.target as HTMLInputElement;
+      unos.value = unos.value.replace(/[^0-9]/g, '')
+  }
+  restrikcijaUnosaBrojeva(event:Event){
+    const unos = event.target as HTMLInputElement;
+    unos.value = unos.value.replace(/[^a-zA-Z ]/g, '')
+  }
+
 }
